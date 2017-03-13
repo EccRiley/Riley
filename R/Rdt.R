3a98ff79770f3471d98efc42996b3c2c0d4bad6e
EccRiley <rms7@pdx.edu>
2017-03-13 13:06:20 -0700

    Adjust default arguments for Rdt()
    Add "keyTable" extension.
    Change class to "display"
    Add JS() function for formatting header row.
    Adjust font formatting for caption.

--- R/Rdt.R
+++ R/Rdt.R
@@ -1,23 +1,31 @@
 #' ## **`Rdt()`**
-#' 
+#'
 #' `DT::datatable()` with my usual settings.^[Thanks to `Stack-Overflow` user [\textbf{\color{dkblue}{Sven Hohenstein}}](http://stackoverflow.com/questions/14577412/how-to-convert-variable-object-name-into-string) for providing the function deparse(substitute(x)) idea to convert an Robject's name to string. Notes: `deparse()` = "Turn unevaluated expressions into character strings"; `substitute()` = "return the parse tree for the unevaluated expression, substituting any variables bound in env."]
 #'
 Rdt <-
     function(x,
              caption = deparse(substitute(x)),
              rownames = FALSE,
-             class = c("row-border"),
+             class = c("display"),
              filter = 'none',
+             extensions = 'KeyTable',
+             selection = list(target = 'row'),
              options = list(
                  pageLength = 300,
                  lengthMenu = c(200, 500, 1000, 1500),
                  info = TRUE,
+                 keys = 'true',
+                 # columnDefs = list(list(className = 'dt-center')),
                  dom = 'tip',
+                 initComplete = DT::JS(
+                     "function(settings, json) {",
+                     "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff', 'font-family': 'serif'});",
+                     "}"),
                  ...
              ),
              ...) {
         cap <-
-            htmltools::tags$caption(style = 'caption-side: top; text-align: left;',
+            htmltools::tags$caption(style = 'caption-side: top; text-align: left; font-size: 1.5em;',
                                     htmltools::strong(htmltools::em(paste(caption))))
         DT::datatable(
             x,
@@ -26,6 +34,7 @@ Rdt <-
             filter = filter,
             class = class,
             options = options,
+            extensions = extensions,
             ...
         )
     }
