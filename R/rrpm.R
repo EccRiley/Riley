dbbc4eabe463449275d20151a906018bfe5a1dbc
EccRiley <rms7@pdx.edu>
2017-03-18 13:55:52 -0700

    Add reversal function ("rrpm()") for "rpm()" function

---
+++ R/rrpm.R
@@ -0,0 +1,6 @@
+rrpm <- function() { ## Re-Replace pander printing methods with default print methods after running "rpm()" ##
+    l <- ls()
+    l.gr <- grepl("print\\..*?", l, perl = TRUE)
+    l.rm <- as.character(l[l.gr == 1])
+    rm(list = l.rm)
+}
