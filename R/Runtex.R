91bba8361e3a27e22f7029640cc28c751ce7943f
EccRiley <rms7@pdx.edu>
2017-05-01 18:18:45 -0700

    Comment-out test examples in Runtex() definition file

--- R/Runtex.R
+++ R/Runtex.R
@@ -1,21 +1,21 @@
-file <- "MAP-v6 copy.Rmd"
-ex <- data.frame(
-	a = c("`\\{\\! (.*?) \\!\\}`", 
-		"\\\\chisq", 
-		"\\\\url\\{(.*?)\\}", 
-		"\\\\%", 
-		"\\\\parindent=\\-1\\.75em", 
-		"\\\\setlength\\{\\\\parskip\\}\\{0\\.25\\\\baselineskip\\}",
-		"\\\\doublespacing",
-		"\\\\Frule"), 
-	b = c("\\1", 
-		"\\\\chi^{2}", 
-		"\\1", 
-		"%", 
-		" ", 
-		" ",
-		" ",
-		" "))
+# file <- "MAP-v6 copy.Rmd"
+# ex <- data.frame(
+# 	a = c("`\\{\\! (.*?) \\!\\}`",
+# 		"\\\\chisq",
+# 		"\\\\url\\{(.*?)\\}",
+# 		"\\\\%",
+# 		"\\\\parindent=\\-1\\.75em",
+# 		"\\\\setlength\\{\\\\parskip\\}\\{0\\.25\\\\baselineskip\\}",
+# 		"\\\\doublespacing",
+# 		"\\\\Frule"),
+# 	b = c("\\1",
+# 		"\\\\chi^{2}",
+# 		"\\1",
+# 		"%",
+# 		" ",
+# 		" ",
+# 		" ",
+# 		" "))
 
 Runtex <- function(file, addlexpr = NULL, cat = TRUE, catFile = NULL, ...) {
 	x <- readLines(file)
@@ -40,4 +40,4 @@ Runtex <- function(file, addlexpr = NULL, cat = TRUE, catFile = NULL, ...) {
 	else return(xnew)
 }
 
-Runtex(file = file, addlexpr = ex, cat = TRUE, catFile = "MAP-v6-untex.Rmd")
+# Runtex(file = file, addlexpr = ex, cat = TRUE, catFile = "MAP-v6-untex.Rmd")
