b566a1f8c68e0c8b596054cd142fad28054b3b7b
EccRiley <rms7@pdx.edu>
2017-07-12 15:39:29 -0700

    Replace default value for "n" argument (14) in Rperc() with getOption("Rperc_n"), add "digits" argument to Rperc(), and remove "tex" argument from Rperc()

--- R/Rperc.R
+++ R/Rperc.R
@@ -1,12 +1,20 @@
 #' ## **`Rperc()`**
 #'
-Rperc <- function(x, n = 14, tex = FALSE) {
-    if (length(x) == 0)
-        return(character())
-    y <- round(x / n, digits = getOption("digits"))
-    if (tex) {
-    	paste0(comma(y * 100), "\\%")
+Rperc <- function(x, n = getOption("Rperc_n"), digits = getOption("digits")) {
+    if (is.null(n)) { stop("argument \"n\" is missing, with no default") }
+    p <- (x / n)*100
+    d <- sub("^.+[.]", "", p)
+    res <- round(p, digits = digits)
+    if (knitr:::is_latex_output()) {
+        apprx <- if (digits < nchar(d)) "\\approx " else ""
+        paste0("\\", apprx, comma(res), "\\%")
     } else {
-    	paste0(comma(y * 100), "%")
+        if (knitr:::is_html_output()) {
+            apprx <- if (digits < nchar(d)) "&asymp; " else ""
+            paste0(apprx, comma(res), "%")
+        } else {
+            apprx <- if (digits < nchar(d)) "~ " else ""
+            paste0(apprx, comma(res), "%")
+        }    	
     }
 }
