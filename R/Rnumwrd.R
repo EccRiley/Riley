72536c3c56b96279cd389a1d7ae1ec713e74babf
EccRiley <rms7@pdx.edu>
2017-07-17 20:56:00 -0700

    Add Rnumwrd.R

---
+++ R/Rnumwrd.R
@@ -0,0 +1,9 @@
+Rnumwrd <- function(x, cap = FALSE) {
+    numWords <- c("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    if (cap == TRUE) {
+        numWords <- sapply(numWords, Riley::RtCap, USE.NAMES = FALSE)
+    }
+    if (x > length(numWords) || (x %% 1) != 0) { stop("'x' is not a single-digit whole number") }
+    res <- numWords[x]
+    return(res)
+}
