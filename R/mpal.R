8ced3d2299930f72b05b08373e0d61cc40cba0b3
EccRiley <rms7@pdx.edu>
2017-07-19 13:33:55 -0700

    Minor adjustments to newly added palette and Ramp function definitions

--- R/mpal.R
+++ R/mpal.R
@@ -2,7 +2,7 @@
 #'
 #' Random color palette generation, using palette specified from '`colorRampPalette(n)`', with '`n`' determined by length of, or number of rows in, '`x`'.
 #'
-mpal <- function(x, p = pal, a = 1, ...) {
+mpal <- function(x, p = ppal, a = 1, ...) {
     if (class(x) == "data.frame")
         l <- nrow(x)
     else
