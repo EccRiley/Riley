589e0031c10d4c85a1e67d4aa219c97649fdb92d
Riley Smith-Hunter <rms7@pdx.edu>
2017-10-10 15:35:11 -0400

    Update Ras.percent.R
    Add option to transform input to percentage

--- R/Ras.percent.R
+++ R/Ras.percent.R
@@ -1,5 +1,10 @@
 ## laziness-inspired wrapper for presenting percentages appropriately, without having to change the numeric class of the data itself (unless you want to, i suppose) ##
 
-Ras.percent <- function(x) {
-    paste0(x, "%")
+Ras.percent <- function(x, compute = FALSE) {
+    if (compute) {
+        y <- x*100
+        res <- paste0(y, "%")
+    } else 
+        res <- paste0(x, "%")
+    return(res)
 }
