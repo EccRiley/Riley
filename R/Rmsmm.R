95af5dda7c3250aad34866082688ee9bd8f4d806
Riley Smith-Hunter <rms7@pdx.edu>
2017-11-29 12:09:50 -0500

    Update Rmsmm() to align with dplyr-0.7.3 updates
    - {summarise_each(xn, ...) --> summarize_if(x, is.numeric, ...)} (also allowed me to remove step to determine numeric columns in input data)
    - Also added stops for non-numeric input data & fixed potential bug when input is a data matrix instead of dataframe.

--- R/Rmsmm.R
+++ R/Rmsmm.R
@@ -3,11 +3,12 @@
 #' A function for creating a quick, simple, mostly report-ready, summary table reporting `mean, sd, min, max` (hence "`msmm`") for the numeric columns in a given table, dataframe, or matrix. The returned value from `R.msmm()` can be passed to `knitr::kable()` for creating a report-ready output table.
 #'
 #'
-#' ## **`R.msmm()`**
+#' ## **`Rmsmm()`**
 #'
 Rmsmm <- function(x, d = 2) {
-	require(dplyr)
+	require(dplyr)#, quietly = TRUE, warn.conflicts = FALSE) ## tempted to make totally quiet, but worried this could cause users issues if not aware of dplyr package loading & corresponding potential conflicts ## 
     if (is.null(ncol(x))) {
+        if (!is.numeric(x)) stop("x must be numeric.");
         xM <- mean(x, na.rm = TRUE)
         xSD <- sd(x, na.rm = TRUE)
         xMIN <- min(x, na.rm = TRUE)
@@ -18,13 +19,15 @@ Rmsmm <- function(x, d = 2) {
         summ <- apply(summ, 2, round, digits = d)
         return(summ)
     } else {
-        nums <- sapply(x, is.numeric)
-        xn <- x[, nums]
-        xM <- dplyr::summarise_each(xn, funs(mean(., na.rm = TRUE)))
-        xSD <- dplyr::summarise_each(xn, funs(sd(., na.rm = TRUE)))
-        xMIN <- dplyr::summarise_each(xn, funs(min(., na.rm = TRUE)))
-        xMAX <- dplyr::summarise_each(xn, funs(max(., na.rm = TRUE)))
-        xNA <- sapply(x, Risna)
+        if (all(sapply(x, is.numeric) == FALSE)) stop("No numeric columns in the data.");
+        if (is.matrix(x)) {
+            x <- as.data.frame(x)
+        }
+        xM <- dplyr::summarise_if(x, is.numeric, funs(mean(., na.rm = TRUE)))
+        xSD <- dplyr::summarise_if(x, is.numeric, funs(sd(., na.rm = TRUE)))
+        xMIN <- dplyr::summarise_if(x, is.numeric, funs(min(., na.rm = TRUE)))
+        xMAX <- dplyr::summarise_if(x, is.numeric, funs(max(., na.rm = TRUE)))
+        xNA <- sapply(x, Riley::Risna)
         summ <- rbind(xM, xSD, xMIN, xMAX, xNA)
         row.names(summ) <- c("M", "SD", "Min", "Max", "NAs")
         summ <- as.data.frame(t(summ))
@@ -32,3 +35,7 @@ Rmsmm <- function(x, d = 2) {
         return(summ)
     }
 }
+
+x <- matrix(letters[1:9], nrow = 3) ## causes error ##
+x <- c(letters[1:9]) ## causes error ##
+x <- matrix(1:9, nrow = 3) ## runs just fine ##
