18641dce610faa5d1242d7a82bf21b31f4c986f2
Riley Smith-Hunter <rms7@pdx.edu>
2017-12-11 13:22:06 -0500

    Create Rtrellispar.R

---
+++ R/Rtrellispar.R
@@ -0,0 +1,17 @@
+Rtrellispar <- function() {
+    trellis.lines <- lattice::trellis.par.get("superpose.line")
+    trellis.lines$col <- Riley::lancet(7)
+    trellis.lines$lwd <- rep(2, 7)
+    lattice::trellis.par.set("superpose.line", trellis.lines)
+    trellis.pts <- lattice::trellis.par.get("superpose.symbol")
+    trellis.pts$col <- trellis.lines$col
+    trellis.pts$fill <- adjustcolor(trellis.pts$col, alpha.f = 0.25)
+    trellis.pts$cex <- 1
+    lattice::trellis.par.set("superpose.symbol", trellis.pts)
+    trellis.strip <- lattice::trellis.par.get("strip.background")
+    trellis.strip$col <- adjustcolor(Riley::grays(7), alpha.f = 0.35)
+    lattice::trellis.par.set("strip.background", trellis.strip)
+    trellis.shingle <- lattice::trellis.par.get("strip.shingle")
+    trellis.shingle$col <- adjustcolor("black", alpha.f = 0.65)
+    lattice::trellis.par.set("strip.shingle", trellis.shingle)
+}
