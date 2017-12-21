c0bc2307a83aaedd45b8a361894b339932cd6af2
Riley Smith-Hunter <rms7@pdx.edu>
2017-12-21 10:27:22 -0500

    Update Rroll.R

--- R/Rroll.R
+++ R/Rroll.R
@@ -1,7 +1,7 @@
 Rroll <- function(x, roll) {
 	rollend <- unique(x)
-	_rollstart <- rollend - (roll - 1)
-	_gp <- grep("^20\\d{2}[(9\\d)(00)]$", _rollstart, perl = TRUE, value = TRUE)
-	rollstart <- ifelse(_rollstart %in% _gp, _rollstart - 88, _rollstart)
-	return(list(rollstart = rollstart, rollend = rollend)
+	rollstart0 <- rollend - (roll - 1)
+	gp <- grep("^20\\d{2}[(9\\d)(00)]$", rollstart0, perl = TRUE, value = TRUE)
+	rollstart <- ifelse(rollstart0 %in% gp, rollstart0 - 88, rollstart0)
+	return(list(rollstart = rollstart, rollend = rollend))
 }
