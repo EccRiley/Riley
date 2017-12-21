49cf6caf7795b59d6a173cfd078e5401e65ad6e3
Riley Smith-Hunter <rms7@pdx.edu>
2017-12-21 10:58:41 -0500

    Fix regex error in Rroll.R

--- R/Rroll.R
+++ R/Rroll.R
@@ -1,7 +1,7 @@
 Rroll <- function(x, roll) {
 	rollend <- unique(x)
 	rollstart0 <- rollend - (roll - 1)
-	gp <- grep("^20\\d{2}[(9\\d)(00)]$", rollstart0, perl = TRUE, value = TRUE)
+	gp <- grep("^20\\d{2}(9\\d)|(00)$", rollstart0, perl = TRUE, value = TRUE)
 	rollstart <- ifelse(rollstart0 %in% gp, rollstart0 - 88, rollstart0)
 	return(list(rollstart = rollstart, rollend = rollend))
 }
