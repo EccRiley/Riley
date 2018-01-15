b7415d5d6b8996fe9deb752fcb7ba526ce260414
Riley Smith-Hunter <rms7@pdx.edu>
2018-01-15 12:17:43 -0500

    Create Rdbq.R

---
+++ R/Rdbq.R
@@ -0,0 +1,11 @@
+Rdbq <- function(dbpath, tables = c("freecode", "treecode", "codecat", "cases", "caselinkage", "source", "coding")) {
+	require(RSQLite); require(DBI)
+	db <- dbConnect(SQLite(), dbpath)
+	tbls <- list(mode = "list", length = length(tables))
+	for (i in 1:length(tables)) {
+		dbGetQuery(db, paste0("SELECT * FROM ", tables[i], ";")) -> tbls[[i]]
+	}
+	dbDisconnect(db)
+	names(tbls) <- tables
+	return(tbls)
+}
