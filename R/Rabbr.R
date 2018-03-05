7b3802b4ec0181d84a248249571172354a7beac0
Riley Smith-Hunter <rms7@pdx.edu>
2018-03-05 10:05:10 -0500

    Update Rabbr.R

--- R/Rabbr.R
+++ R/Rabbr.R
@@ -1,24 +1,23 @@
-Rabbr <- function(x) {
+Rabbr <- function(x, stopwords = c("a", "the", "to", "at", "in", "with", "and", "but", "or", "of", "&"), toupper = TRUE) {
     s0 <- strsplit(x, " ")[[1]]
-    ex <- c("a", "the", "to", "at", "in", "with", "and", "but", "or", "of", "&")
-    s1 <- s0[!s0 %in% ex]
-    s2 <- substring(s1, 1,1)
-    s <- paste(s2, sep = "", collapse = "")
-    s <- toupper(s)
+    s1 <- s0[!s0 %in% stopwords]
+    s2 <- substring(s1, 1, 1)
+    s3 <- paste(s2, sep = "", collapse = "")
+    s <- ifelse(toupper == TRUE, toupper(s3), s3)
     return(s)
 }
 
 #' Examples
 
-x <- c("Journal of Interpersonal Violence",
+# x <- c("Journal of Interpersonal Violence",
          "Violence Against Women",
          "Violence and Victims",
          "Journal of Family Violence")
-sapply(x, Rabbr)
-sapply(x, Rabbr, USE.NAMES = FALSE)
+# sapply(x, Rabbr)
+# sapply(x, Rabbr, USE.NAMES = FALSE)
 
-xlow <- sapply(x, tolower, USE.NAMES = FALSE)
-xlow
+# xlow <- sapply(x, tolower, USE.NAMES = FALSE)
+# xlow
 
-sapply(xlow, Rabbr)
-sapply(xlow, Rabbr, USE.NAMES = FALSE)
+# sapply(xlow, Rabbr)
+# sapply(xlow, Rabbr, USE.NAMES = FALSE)
