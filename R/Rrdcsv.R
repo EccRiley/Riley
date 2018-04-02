37319d22936fadbcf6901f1f24af4ebad8896dc7
Riley Smith-Hunter <rms7@pdx.edu>
2018-04-02 11:01:29 -0400

    Update Rrdcsv.R
    Add capabilities to read additional formats, assuming the appropriate packages are installed: c(".csv", ".dat", ".xlsx", ".feather")

--- R/Rrdcsv.R
+++ R/Rrdcsv.R
@@ -1,5 +1,17 @@
-Rrdcsv <- function(x, namecase = "lower", ...) {
-    y <- read.csv(x, ...)
+Rrdcsv <- function(x, ext = "csv", namecase = "lower", ...) {
+    if (ext == "feather") {
+        y <- feather::read_feather(x, ...)
+    } else {
+        if (ext == "xlsx") {
+            y <- xlsx::read_feather(x, ...)
+        } else {
+            if (ext == "csv") {
+                y <- read.csv(x, ...)
+            } else {
+                y <- read.table(x, ...)
+            }
+        }
+    }
     if (namecase == "lower") {
         names(y) <- tolower(names(y))
     } else if (namescase == "upper") {
