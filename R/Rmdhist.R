540c188a592563abb90298159fbadb5b85e256c8
Riley Smith-Hunter <rms7@pdx.edu>
2018-04-03 14:46:23 -0400

    Update Rmdhist.R

--- R/Rmdhist.R
+++ R/Rmdhist.R
@@ -1,6 +1,6 @@
-Rmdhist <- function(x, nrow, ncol, pal = mpal(seq(1:ncol(x)), p = cols3), labs = colnames(x), xlab = labs, main = paste0("Histogram of ", labs), ...) {
+Rmdhist <- function(x, width = ceiling(sqrt(ncol(x))), length = ceiling(ncol(x)/width), pal = mpal(seq(1:ncol(x)), p = cols3), labs = colnames(x), xlab = labs, main = paste0("Histogram of ", labs), ...) {
     origPar <- par("mfrow")
-    par(mfrow = c(nrow, ncol))
+    par(mfrow = c(length, width))
     for (i in 1:ncol(x)) {
         hist(x[, i], col = pal[i], freq = FALSE, main = main[i], xlab = xlab[i], ...);
         lines(density(x[, i], na.rm = TRUE), lwd = 1, lty = "dotted", ...)
