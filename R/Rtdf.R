c1440dcbbfdb07a84149d6895f0fd1c0c5c9d18f
Riley Smith-Hunter <rms7@pdx.edu>
2018-04-18 14:27:44 -0400

    Update Rtdf.R

--- R/Rtdf.R
+++ R/Rtdf.R
@@ -5,12 +5,13 @@
 #'
 #' "..." == additional vectors to table against 'x'
 #'
-Rtdf <- function(x, ..., names = c(deparse(substitute(x)), "Freq"), cls = "df") {
-	d <- c("data.frame", "df")
+Rtdf <- function(x, ..., names = c(deparse(substitute(x)), "Freq"), cls = "dt") {
+	d <- c("dt", "data.table", "data.frame", "df")
 	m <- c("matrix", "mat")
 	l <- c("list", "ls")
 	if (cls %in% d) {
-		t <- as.data.frame(table(x, ...))
+		require(data.table)
+		t <- as.data.table(table(x, ...))
 		names(t) <- names
 	} else if (cls %in% m) {
 		t <- as.matrix(table(x, ...))
@@ -18,6 +19,6 @@ Rtdf <- function(x, ..., names = c(deparse(substitute(x)), "Freq"), cls = "df") 
 	} else if (cls %in% l) {
 		t <- as.list(table(x, ...))
 	} else 
-	stop("cls must be either 'data.frame'/'df', 'matrix'/'mat', or 'list'/'ls'")
+	stop("cls must be either 'data.table'/'data.frame'/'dt'/'df', 'matrix'/'mat', or 'list'/'ls'")
 	return(t)
 }
