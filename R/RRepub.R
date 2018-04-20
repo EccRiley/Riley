88669737967a281fe30b7750b46dce55197f32f6
Riley Smith-Hunter <rms7@pdx.edu>
2018-04-20 14:27:39 -0400

    Delete RRepub.R

--- R/RRepub.R
+++
@@ -1,7 +0,0 @@
-RRepub <-
-    function(x, ...) {
-        rmarkdown::render(x, output_format = "bookdown::epub_book", ...)
-        p <- gsub("\\.Rmd", "\\.epub", x)
-        bookdown::kindlegen(p)
-        print(format(Sys.time(), '%n[%d %b. %I:%M%p]'))
-    }
