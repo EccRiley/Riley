321de9b970215a50bb746a0ebee798b72fba964b
Riley Smith-Hunter <rms7@pdx.edu>
2018-04-23 12:28:55 -0400

    Update thm_Rwsj.R

--- R/thm_Rwsj.R
+++ R/thm_Rwsj.R
@@ -1,12 +1,33 @@
 
-thm_Rwsj <- function(base_size = 10, base_family = "serif", title_family = "serif", bgcolor = "white", legend.position = "bottom", legend.direction = "horizontal", ...) {
+thm_Rwsj <- function(base_size = 10, base_family = "serif", title_family = "serif", bgcolor = "white", 
+                     rect = element_rect(fill = NA, linetype = 1, colour = "#474747", size = 0.5),
+                     title = element_text(size = rel(1.25), color = "#474747", face = "bold"),
+                     axis.title = element_text(size = rel(1), face = "bold", color = "#474747"),
+                     axis.text = element_text(size = rel(0.75), face = "plain"),
+                     legend.position = "bottom", 
+                     legend.direction = "horizontal", 
+                     legend.title = element_text(size = rel(1), face = "bold.italic", color = "#474747"),
+                     legend.text = element_text(size = rel(1)),
+                     legend.key = element_rect(color = NA),
+                     legend.key.size = unit(0.5, "cm"), 
+                     strip.background = element_rect(fill = "#e3e3e6"),
+                     strip.text = element_text(face = "bold"),
+                     ...) {
     require(ggplot2); require(ggthemes);
     thm <- theme_wsj(base_size, base_family, title_family, color = bgcolor) %+replace% 
-        theme(title = element_text(size = rel(2)), 
-              axis.text = element_text(size = rel(1)), 
-              legend.title = element_text(size = rel(1.15)),
-              legend.text = element_text(size = rel(1)),
-              legend.position = legend.position, legend.direction = legend.direction,
+        theme(rect = rect,
+              title = title, 
+              axis.title = axis.title,
+              axis.text = axis.text, 
+              legend.position = legend.position, 
+              legend.direction = legend.direction,
+              legend.title = legend.title, 
+              legend.text = legend.text,
+              legend.key = legend.key,
+              legend.key.size = legend.key.size,
+              strip.background = strip.background,
+              strip.text = strip.text,
+              # panel.background = element_rect(color = "#474747", size = 1),
               ...)
     thm
 }
