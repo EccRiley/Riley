7cd75a5238fcf574298e93c3b199990c2cd6563c
Riley Smith-Hunter <rms7@pdx.edu>
2018-04-23 10:18:40 -0400

    Update thm_Rwsj.R

--- R/thm_Rwsj.R
+++ R/thm_Rwsj.R
@@ -1,12 +1,12 @@
 
-thm_Rwsj <- function(base_size = 10, base_family = "serif", title_family = "serif", bgcolor = "white", legend.position = "bottom", ...) {
+thm_Rwsj <- function(base_size = 10, base_family = "serif", title_family = "serif", bgcolor = "white", legend.position = "bottom", legend.direction = "horizontal", ...) {
     require(ggplot2); require(ggthemes);
-	thm <- theme_wsj(base_size, base_family, title_family, color = bgcolor) %+replace% 
-	theme(title = element_text(size = rel(2)), 
-		axis.text = element_text(size = rel(1)), 
-		legend.title = element_text(size = rel(1.25)),
-		legend.text = element_text(size = rel(1)),
-		legend.position = legend.position,
-	...)
-	thm
+    thm <- theme_wsj(base_size, base_family, title_family, color = bgcolor) %+replace% 
+        theme(title = element_text(size = rel(2)), 
+              axis.text = element_text(size = rel(1)), 
+              legend.title = element_text(size = rel(1.15)),
+              legend.text = element_text(size = rel(1)),
+              legend.position = legend.position, legend.direction = legend.direction,
+              ...)
+    thm
 }
