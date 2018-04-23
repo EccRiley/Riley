76f42f29cbe8d69400386dd25cc5554c4d5465ee
Riley Smith-Hunter <rms7@pdx.edu>
2018-04-23 12:31:29 -0400

    Update thm_Rwsj.R

--- R/thm_Rwsj.R
+++ R/thm_Rwsj.R
@@ -1,6 +1,5 @@
 
-thm_Rwsj <- function(base_size = 10, base_family = "serif", title_family = "serif", bgcolor = "white", 
-                     rect = element_rect(fill = NA, linetype = 1, colour = "#474747", size = 0.5),
+thm_Rwsj <- function(base_size = 10, base_family = "serif", title_family = "serif", bgcolor = "white",
                      title = element_text(size = rel(1.25), color = "#474747", face = "bold"),
                      axis.title = element_text(size = rel(1), face = "bold", color = "#474747"),
                      axis.text = element_text(size = rel(0.75), face = "plain"),
@@ -15,8 +14,7 @@ thm_Rwsj <- function(base_size = 10, base_family = "serif", title_family = "seri
                      ...) {
     require(ggplot2); require(ggthemes);
     thm <- theme_wsj(base_size, base_family, title_family, color = bgcolor) %+replace% 
-        theme(rect = rect,
-              title = title, 
+        theme(title = title, 
               axis.title = axis.title,
               axis.text = axis.text, 
               legend.position = legend.position, 
@@ -27,6 +25,7 @@ thm_Rwsj <- function(base_size = 10, base_family = "serif", title_family = "seri
               legend.key.size = legend.key.size,
               strip.background = strip.background,
               strip.text = strip.text,
+              rect = element_rect(linetype = 1, colour = "#474747"),
               # panel.background = element_rect(color = "#474747", size = 1),
               ...)
     thm
