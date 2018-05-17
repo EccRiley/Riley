00ea84935d7f65655954d131f7748a0b497e4434
Riley Smith-Hunter <rms7@pdx.edu>
2018-05-17 14:07:13 -0400

    Update Rtdf.R

--- R/Rtdf.R
+++ R/Rtdf.R
@@ -5,20 +5,22 @@
 #'
 #' "..." == additional vectors to table against 'x'
 #'
-Rtdf <- function(x, ..., names = c(deparse(substitute(x)), "Freq"), cls = "dt") {
-	d <- c("dt", "data.table", "data.frame", "df")
-	m <- c("matrix", "mat")
-	l <- c("list", "ls")
-	if (cls %in% d) {
-		require(data.table)
-		t <- as.data.table(table(x, ...))
-		names(t) <- names
-	} else if (cls %in% m) {
-		t <- as.matrix(table(x, ...))
-		colnames(t) <- names
-	} else if (cls %in% l) {
-		t <- as.list(table(x, ...))
-	} else 
-	stop("cls must be either 'data.table'/'data.frame'/'dt'/'df', 'matrix'/'mat', or 'list'/'ls'")
-	return(t)
+
+Rtdf <- function(x, ..., names = c(deparse(substitute(x)), "Freq"), cls = "dt", 
+                 exclude = if (useNA == "no") c(NA, NaN), useNA = c("no", "ifany", "always")) {
+    d <- c("dt", "data.table", "data.frame", "df")
+    m <- c("matrix", "mat")
+    l <- c("list", "ls")
+    if (cls %in% d) {
+        require(data.table)
+        t <- as.data.table(table(x, ..., exclude = exclude, useNA = useNA))
+        names(t) <- names
+    } else if (cls %in% m) {
+        t <- as.matrix(table(x, ..., exclude = exclude, useNA = useNA))
+        colnames(t) <- names
+    } else if (cls %in% l) {
+        t <- as.list(table(x, ..., exclude = exclude, useNA = useNA))
+    } else 
+        stop("cls must be either 'data.table'/'data.frame'/'dt'/'df', 'matrix'/'mat', or 'list'/'ls'")
+    return(t)
 }
