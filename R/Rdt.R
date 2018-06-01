65523337a2daa7118c2d8251feb32f6df8274287
Riley Smith-Hunter <rms7@pdx.edu>
2018-06-01 12:22:05 -0400

    Update Rdt.R

--- R/Rdt.R
+++ R/Rdt.R
@@ -1,52 +1,122 @@
-#' ## **`Rdt()`**
+#' ## **`Rdt()` & `Rdt2()`**
 #'
-#' `DT::datatable()` with my usual settings.^[Thanks to `Stack-Overflow` user [\textbf{\color{dkblue}{Sven Hohenstein}}](http://stackoverflow.com/questions/14577412/how-to-convert-variable-object-name-into-string) for providing the function deparse(substitute(x)) idea to convert an Robject's name to string. Notes: `deparse()` = "Turn unevaluated expressions into character strings"; `substitute()` = "return the parse tree for the unevaluated expression, substituting any variables bound in env."]
+#' **`Rdt()`**: DT::datatable()` with my usual settings.^[Thanks to `Stack-Overflow` user [\textbf{\color{dkblue}{Sven Hohenstein}}](http://stackoverflow.com/questions/14577412/how-to-convert-variable-object-name-into-string) for providing the function deparse(substitute(x)) idea to convert an Robject's name to string. Notes: `deparse()` = "Turn unevaluated expressions into character strings"; `substitute()` = "return the parse tree for the unevaluated expression, substituting any variables bound in env."]
 #'
 Rdt <-
     function(x,
              caption = deparse(substitute(x)),
              rownames = FALSE,
              class = c("display"),
-             filter = 'none',
-             extensions = 'KeyTable',
-             selection = list(target = 'row'),
-             columnDefs = NULL, #EXAMPLE: columnDefs = list(list(className = 'dt-center', targets = 0:4)
-             options = list(
-                 pageLength = 300,
-                 lengthMenu = c(200, 500, 1000, 1500),
-                 info = TRUE,
-                 keys = 'true',
-                 columnDefs = columnDefs,
-                 dom = 'tip',
-                 initComplete = DT::JS(
-                     "function(settings, json) {",
-                     "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff', 'font-family': 'serif'});",
-                     "}")
-             ),
+             height = 300,
+             filter = "top",
+             extensions = "KeyTable",
+             selection = list(target = "row"),
+             opts.columnDefs = NULL, #EXAMPLE: columnDefs = list(list(className = "dt-center", targets = 0:4)
+             opts.pageLength = 10,
+             opts.lengthMenu = c(10, 25, 50, 100, 200, 500, 1000, 1500),
+             opts.info = TRUE,
+             opts.keys = "true",
+             opts.dom = "tip", 
+             opts.initComplete = DT::JS(
+                 "function(settings, json) {",
+                 "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff', 'font-family': 'consolas'});",
+                 "}"),
+             opts.other = list(), 
              ...) {
-        cap <-
-            htmltools::tags$caption(style = 'caption-side: top; text-align: left; font-size: 1.5em;',
-                                    htmltools::strong(htmltools::em(paste(caption))))
+        
+        cap <- htmltools::tags$caption(style = 'caption-side: top; text-align: left; font-size: 1.25em;',
+                                       htmltools::strong(htmltools::em(paste(caption)))
+        )
+        
+        opts <- as.list(c(
+            pageLength = opts.pageLength,
+            lengthMenu = opts.lengthMenu,
+            info = opts.info,
+            keys = opts.keys,
+            columnDefs = opts.columnDefs,
+            dom = opts.dom,
+            initComplete = opts.initComplete, 
+            unlist(opts.other))
+        )
+        
         DT::datatable(
             x,
             rownames = rownames,
             caption = cap,
+            height = height,
             filter = filter,
             class = class,
-            options = options,
+            options = opts,
             extensions = extensions,
             ...
         )
     }
 #'
-
-Rdt2 <- function(x, cnames = colnames(x), rnames = FALSE, align = NULL, digits = 2, format.args = list(), escape = TRUE, table.attr = NULL, columnDefs = NULL, ...) {
+#' 
+#' 
+#' **`Rdt2()`**: (mostly a convenience function) Conditionally implement `Rdt()` (i.e., `HTML datatable` output) or knitr::kable(format = 'pandoc') depending on session mode (interactive vs. batch) and, when running `rmarkdown::render()`, the rendered document output format (i.e., `HTML` output formats will trigger implmentation of `Rdt()`, wherease non-`HTML` otuput formats will trigger `knitr::kable(...)` to be implemented).
+#' 
+Rdt2 <- function(x,
+                 ## ARGS FOR EITHER Rdt() & kable() OUTPUTS ##
+                 cnames = colnames(x), rnames = FALSE, escape = TRUE, caption = deparse(substitute(x)),
+                 
+                 ## ARGS SPECIFIC TO kable() OUTPUT ##
+                 format = getOption("knitr.table.format"), 
+                 zero.print = ".", 
+                 digits = 2, 
+                 nsmall = 2,
+                 kable.other.f.args = list(),
+                 align = NULL, 
+                 table.attr = NULL,
+                 
+                 ## ARGS FOR Rdt() OUTPUT ##
+                 height = 300,
+                 filter = "top",
+                 extensions = "KeyTable",
+                 selection = list(target = "row"),
+                 opts.columnDefs = NULL, 
+                 opts.pageLength = 300,
+                 opts.lengthMenu = c(200, 500, 1000, 1500),
+                 opts.info = TRUE,
+                 opts.keys = "true",
+                 opts.dom = "tip", 
+                 opts.initComplete = DT::JS(
+                     "function(settings, json) {",
+                     "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff', 'font-family': 'consolas'});",
+                     "}"), 
+                 opts.other = list(), 
+                 ...) { ## '...' CURRENTLY ONLY IMPLEMENTED AS ADDL ARGS PASSED TO Rdt() TO AVOID POTENTIAL 'UNUSED ARGUMENT' ERRORS WITH 'knitr::kable()' ##
+    
+    kable.format.args <- as.list(c(zero.print = kable.zero.print,
+                                digits = kable.digits,
+                                nsmall = kable.nsmall,
+                                unlist(kable.other.f.args)))
     if (knitr:::is_html_output()) {
-        Rdt(x, colnames = cnames, rownames = rnames, escape = escape, columnDefs = columnDefs, ...)
+        Rdt(x, colnames = cnames, rownames = rnames, escape = escape, caption = caption,
+            height = height,
+            filter = filter,
+            extensions = extensions,
+            selection = selection,
+            opts.columnDefs = opts.columnDefs,
+            opts.pageLength = opts.pageLength,
+            opts.lengthMenu = opts.lengthMenu,
+            opts.info = opts.info,
+            opts.keys = opts.keys,
+            opts.dom = opts.dom,
+            opts.initComplete = opts.initComplete,
+            opts.other,
+            ...)
     } else
-        kable(x, col.names = cnames, row.names = rnames, align = align, digits = digits, 
-              format.args = format.args, escape = escape, table.attr = table.attr, ...)
+        knitr::kable(x, col.names = cnames, row.names = rnames, escape = escape, caption = caption,
+              format = format,
+              format.args = format.args,
+              align = align, 
+              table.attr = table.attr)
 }
+
+
+# CODEDUMP (Rdt() & Rdt2()) -----------------------------------------------
+
 # Rdt2 <- function(x, rownames = FALSE, colnames = NA, caption = deparse(substitute(x)), 
 #                  dt.options = NULL, kbl.format.args = NULL, kbl.align = NA, ...) {
 #     if (knitr:::is_html_output() || interactive()) {
