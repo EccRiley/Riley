23e47941230cc7ee379f01c4910c2f095cee15e5
Riley Smith-Hunter <rms7@pdx.edu>
2018-06-20 09:36:17 -0400

    Update Riley.R

--- R/Riley.R
+++ R/Riley.R
@@ -1,4 +1,4 @@
-#' A working R-library of functions, data, color palettes (etc.) for everyday, and not-so-everyday, R tasks.
+#' A working R-library of functions, data, color palettes (etc.) for everyday, and not-so-everyday, R tasks. This is primarily a 'catch-all' repository of functions, data, and color palettes that I intend to include in one or more 'formal' R libraries (i.e., fully documented and subsequently published on CRAN).
 #'
 #' @name Riley
 #' @import data.table
