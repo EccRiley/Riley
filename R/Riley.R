8efb505fc2ea6ce6caed2a93d1b8bd635a185e58
Riley Smith-Hunter <rms7@pdx.edu>
2018-06-20 09:35:30 -0400

    Create Riley.R

---
+++ R/Riley.R
@@ -0,0 +1,21 @@
+#' A working R-library of functions, data, color palettes (etc.) for everyday, and not-so-everyday, R tasks.
+#'
+#' @name Riley
+#' @import data.table
+#' @import pander
+#' @import grid
+#' @import ggplot2
+#' @import ggthemes
+#' @import ggsci
+#' @import gtable
+#' @import knitr
+#' @import scales
+#' @import plyr
+#' @import dplyr
+#' @import stringr
+#' @import tufte
+#' @import car
+#' @import rmarkdown
+#'
+#' @docType package
+NULL
