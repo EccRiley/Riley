ef7bc13b75adad0da50e41325134701d250f2ab9
Riley Smith-Hunter <rms7@pdx.edu>
2018-09-20 13:14:09 -0400

    Update Rcurrentdata.R

--- R/Rcurrentdata.R
+++ R/Rcurrentdata.R
@@ -3,14 +3,23 @@
 ## [TODO]: IMPLEMENT MORE GENERALIZABLE FUNCTIONALITY THAT IS LESS DEPENDENT ON THE USER'S WORKFLOW (I.E., SCAN FOR THE MOST RECENTLY MODIFIED/ADDED (BASED ON USER-SPECIFIED OPTIONS [TODO]) FILE(S) AND/OR FILES MODIFIED/ADDED WITHIN A PARTICULAR *DATE RANGE* BY PULLING IN THE FILE DETAILS (INSTEAD OF RELYING ON THE FILENAMES ONLY, WHICH IS CURRENTLY THE MAIN BARRIER IN THE FUNCTION'S GENERALIZABILITY) ##
 ## [TODO]: GENERALIZE SO THAT MORE THAN JUST '.csv' FILES CAN BE READ/SEARCHED FOR ##
 
-Rcurrentdata <- function(pattern, path = "data", perl = TRUE, ...) {
-    path <- gsub("^(.*?)/$", "\\1", path, perl = TRUE) ## PREEMPTIVELY AVOID UNINTENTIONALLY DUPLICATED OR MISSING "/"s IN NON-DEFAULT/USER-SPECIFIED PATH STRING ##
+Rcurrentdata <-  function(pattern, path = "data", perl = TRUE, readData = TRUE, ...) {
+    path <- gsub("^(.*?)/$", "\\1", path, perl = TRUE)
     datafiles <- list.files(path = path, pattern = pattern)
-    current <- max(as.integer(gsub(pattern, "\\1", datafiles, perl = perl)))
+    current <- max(as.integer(gsub(pattern, "\\1", datafiles,
+                                   perl = perl)))
     infile <- grep(as.character(current), datafiles, value = TRUE)
     inpath <- paste0(path, "/", infile)
-    message(paste0("## CURRENT DATAFILE = \"", inpath,"\" ##"))
-    currentdata <- Riley::Rrdcsv(inpath, ...)
+    if (readData) {
+        if (infile %in% grep("\\.csv", infile, value = TRUE)) {
+            currentdata <- Riley::Rrdcsv(inpath, ...)
+        } else if (infile %in% grep("\\.xl", infile, value = TRUE)) {
+            currentdata <- readxl::read_xlsx(inpath, ...)
+        } else stop("Unable to read data")
+        message(paste0("## CURRENT DATAFILE = \"", inpath, "\" ##"))
+    } else {
+        currentdata <- inpath
+    }
     return(currentdata)
 }
 
