d9cf5a497fd8e941d7d331c11673176ceff456a8
Riley Smith-Hunter <rms7@pdx.edu>
2018-09-30 17:17:08 -0400

    Update Rgrad.R

--- R/Rgrad.R
+++ R/Rgrad.R
@@ -1,7 +1,7 @@
 #' ## **`Rgrad()`**
 #'
-#' Gradient generation, using palette specified from '`colorRampPalette(n)`', with '`n`' determined by length of, or number of rows in, '`x`'
 #'
+#' Monochromatic gradient generation
 Rgrad <- function(x, p = cols1, alpha = 1) {
     if (class(x) == "data.frame")
         l <- nrow(x)
@@ -9,4 +9,16 @@ Rgrad <- function(x, p = cols1, alpha = 1) {
         l <- length(x)
     adjustcolor(p(l), alpha.f = alpha)
 }
+
+#' Gradient generation, using palette specified from '`colorRampPalette(n)`', with '`n`' determined by length of, or number of rows in, '`x`'
+Rgrad_rainbow <- function(x, start = 0.25, col = pp[20]) {
+    if (is.null(dim(x))) { 
+        sapply(seq(start, 1, length.out = length(x)), 
+                   function(z) adjustcolor(col, alpha.f = z))
+    } else {
+        sapply(seq(start, 1, length.out = dim(x)[[1]]), 
+                   function(z) adjustcolor(col, alpha.f = z))
+    }
+}
+
 #'
