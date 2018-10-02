76bf97da2f3a932440d55f47cd22a14a843cf967
Riley Smith-Hunter <rms7@pdx.edu>
2018-10-02 13:24:47 -0400

    Update Rmsmm.currency.R

--- R/Rmsmm.currency.R
+++ R/Rmsmm.currency.R
@@ -1,15 +1,19 @@
 Rmsmm.currency <-
-    function(x, vars_lab, kable = TRUE, caption = NULL,
+    function(x, currency_symbol = "$", vars_lab, kable = TRUE, caption = NULL,
              outnames = c(vars_lab, "M", "SD", "Min", "Max", "$N_{Unique Values}$", "$N_{Missing}$"), ...) {
         ## '...' = ADD'L ARGS PASSED TO KABLE() (IF KABLE == TRUE) ##
         require(data.table); require(magrittr)
         res <- Rmsmm(x) %>%
             as.data.table() %>%
             transform("x" = paste0("`", names(which(sapply(x, is.numeric))), "`"),
-                      M = ifelse(M < 0, paste0("-$", scales::comma(abs(M))), paste0("$", scales::comma(M))),
-                      SD = ifelse(SD < 0, paste0("-$", scales::comma(abs(SD))), paste0("$", scales::comma(SD))),
-                      Min = ifelse(Min < 0, paste0("-$", scales::comma(abs(Min))), paste0("$", scales::comma(Min))),
-                      Max = ifelse(Max < 0, paste0("-$", scales::comma(abs(Max))), paste0("$", scales::comma(Max))),
+                      M = ifelse(M < 0, paste0("-", currency_symbol, scales::comma(abs(M))),
+                                 paste0(currency_symbol, scales::comma(M))),
+                      SD = ifelse(SD < 0, paste0("-", currency_symbol, scales::comma(abs(SD))),
+                                  paste0(currency_symbol, scales::comma(SD))),
+                      Min = ifelse(Min < 0, paste0("-", currency_symbol, scales::comma(abs(Min))),
+                                   paste0(currency_symbol, scales::comma(Min))),
+                      Max = ifelse(Max < 0, paste0("-", currency_symbol, scales::comma(abs(Max))),
+                                   paste0(currency_symbol, scales::comma(Max))),
                       N_Unique = scales::comma(N_Unique),
                       NAs = scales::comma(NAs)) %>%
             setcolorder("x")
