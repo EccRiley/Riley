cf168d09a97ddbec7193dc92ac951de86fc72820
Riley Smith-Hunter <rms7@pdx.edu>
2018-10-03 15:21:27 -0400

    Update Runiqueness.R

--- R/Runiqueness.R
+++ R/Runiqueness.R
@@ -3,6 +3,6 @@ Runiqueness <- function(df, lab, ...) {
                paste0(round(100*(sapply(df, function(x) length(unique(x)))/nrow(df)), 2), "\\%")) %>%
         kable(caption = paste0("Uniqueness by Column in ", lab, " ($N_{rows}$ total = ",
                                scales::comma(nrow(df)), ")"),
-              col.names = c("$N_{UniqueValues}$", "$%_{rows}"),
+              col.names = c("$N_{UniqueValues}$", "$%_{rows}$"),
               align = "r")
 }
