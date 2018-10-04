e74e42d455ffe33306011badf3bed3c2f3bfefb2
Riley Smith-Hunter <rms7@pdx.edu>
2018-10-04 10:30:28 -0400

    Update Rmdhist.R

--- R/Rmdhist.R
+++ R/Rmdhist.R
@@ -1,4 +1,4 @@
-Rmdhist <- function(x, width = ceiling(sqrt(ncol(x))), length = ceiling(ncol(x)/width), pal = mpal(seq(1:ncol(x)), p = pp), labs = colnames(x), xlab = labs, main = NULL, plotTitle = NULL, outerTitle = TRUE, ...) {
+Rmdhist <- function(x, width = ceiling(sqrt(ncol(x))), length = ceiling(ncol(x)/width), pal = mpal(seq(1:ncol(x)), p = ppal), labs = colnames(x), xlab = labs, main = NULL, plotTitle = NULL, outerTitle = TRUE, ...) {
     par(mfrow = c(length, width))
     for (i in 1:ncol(x)) {
         hist(x[, i], col = pal[i], freq = FALSE, main = main[i], xlab = xlab[i], ...);
