a2d261a25cc2428a7c10b134ca7bd90e07748095
Riley Smith-Hunter <rms7@pdx.edu>
2018-10-04 10:20:04 -0400

    Update Rmdhist.R

--- R/Rmdhist.R
+++ R/Rmdhist.R
@@ -1,8 +1,9 @@
-Rmdhist <- function(x, width = ceiling(sqrt(ncol(x))), length = ceiling(ncol(x)/width), pal = mpal(seq(1:ncol(x)), p = cols3), labs = colnames(x), xlab = labs, main = paste0("Histogram of ", labs), ...) {
+Rmdhist <- function(x, width = ceiling(sqrt(ncol(x))), length = ceiling(ncol(x)/width), pal = mpal(seq(1:ncol(x)), p = pp), labs = colnames(x), xlab = labs, main = NULL, plotTitle = NULL, outerTitle = TRUE, ...) {
     par(mfrow = c(length, width))
     for (i in 1:ncol(x)) {
         hist(x[, i], col = pal[i], freq = FALSE, main = main[i], xlab = xlab[i], ...);
         lines(density(x[, i], na.rm = TRUE), lwd = 1, lty = "dotted", ...)
+        if (!is.null(title)) title(plotTitle, outer = outerTitle)
     }
     on.exit(par(mfrow = c(1, 1))) ## NEED TO FIX THIS ##
 }
