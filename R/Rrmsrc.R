3da41c87f313099b9c729443fe32e0f8b66f633c
Riley Smith-Hunter <rms7@pdx.edu>
2018-10-04 10:16:44 -0400

    Delete Rrmsrc.R

--- R/Rrmsrc.R
+++
@@ -1,5 +0,0 @@
-Rrmsrc <- function(x, ...) { ## BE CAREFUL WITH THIS!!! ##
-    objs <- ls(pos = ".GlobalEnv")
-    rm(list = objs, pos = ".GlobalEnv") ## REMOVE EVERYTHING FROM THE CURRENT SESSION GLOBAL ENVIRONMENT (see `?ls` & `?rm`)
-    source(x)
-}
