e4f912d78ca06c38993ad05fcea8f5a98caa2bca
Riley Smith-Hunter <rms7@pdx.edu>
2018-10-18 18:57:46 -0400

    Update Rfmt.R

--- R/Rfmt.R
+++ R/Rfmt.R
@@ -24,21 +24,22 @@ Rfmt.currency <- function(x, currency_symbol = "$", digits = 2, nsmall = digits,
 
 
 ## RFMT.ABBR() ABBREVIATES LARGE NUMBERS ##
-## BASED ON https://gist.github.com/RadGH/84edff0cc81e6326029c#gistcomment-2095834 ##
-
-## BASED ON https://gist.github.com/RadGH/84edff0cc81e6326029c#gistcomment-2095834 ##
+## VECTORIZED R-VERSION OF: https://gist.github.com/RadGH/84edff0cc81e6326029c#gistcomment-2095834 ##
 Rfmt.abbr <- function(n) {
-    ifelse(n < 1000, 
+    n_abs <- abs(n)
+    ifelse(n_abs < 1000, 
            floor(n),
-           ifelse (n >= 1000 & n < 1000000, 
+           ifelse (n_abs >= 1000 & n_abs < 1000000, 
                    paste0(floor(n / 1000), "K+"),
-                   ifelse (n >= 1000000 & n < 1000000000, 
+                   ifelse (n_abs >= 1000000 & n_abs < 1000000000, 
                            paste0(floor(n / 1000000), "M+"),
-                           ifelse (n >= 1000000000 & n < 1000000000000, 
+                           ifelse (n_abs >= 1000000000 & n_abs < 1000000000000, 
                                    paste0(floor(n / 1000000000), "B+"),
-                                   ifelse(n >= 1000000000000, 
+                                   ifelse(n_abs >= 1000000000000, 
                                           paste0(floor(n / 1000000000000), "T+"),
                                           NA
                                    )))))
 }
+
+
 
