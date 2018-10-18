5741a8c8325d6c3ece3c53009a6ef75b13bd238b
Riley Smith-Hunter <rms7@pdx.edu>
2018-10-18 18:19:41 -0400

    Update Rfmt.R

--- R/Rfmt.R
+++ R/Rfmt.R
@@ -1,19 +1,24 @@
+
 Rfmt <- function(x, digits = 2, nsmall = digits, scientific = FALSE, big.mark = ",", ...) {
-    if (!is.numeric(x) | !is.integer(x) | !is.double(x)) warning("Coercing non-numeric input (x) to numeric")
+    if (!is.numeric(x) | !is.integer(x) | !is.double(x)) {
+        warning("Coercing non-numeric input (x) to numeric")
+    }
     res <- format(round(as.numeric(x), digits = digits), ...,
-           big.mark = big.mark,
-           scientific = scientific,
-           nsmall = digits)
-    res <- ifelse(is.na(x), NA, res)
+                  big.mark = big.mark,
+                  scientific = scientific,
+                  nsmall = digits)
+    res <- ifelse(is.na(as.numeric(x)), NA, res)
     return(res)
 }
 
-Rfmt.currency <- function(x, currency_symbol = "$", digits = 2, nsmall = digits, 
+Rfmt.currency <- function(x, currency_symbol = "$", digits = 2, nsmall = digits,
                           scientific = FALSE, big.mark = ",", ...) {
     y <- paste0(currency_symbol,
                 Rfmt(x, digits = digits, ...,
                      big.mark = big.mark,
                      scientific = scientific,
                      nsmall = digits))
+    y <- ifelse(is.na(as.numeric(x)), NA, y)
     return(y)
 }
+
