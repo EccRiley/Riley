19df2b4ee07b7798aca7cca0367bb3f2b5a582d1
Riley Smith-Hunter <rms7@pdx.edu>
2018-10-18 18:36:24 -0400

    Update Rfmt.R

--- R/Rfmt.R
+++ R/Rfmt.R
@@ -22,3 +22,32 @@ Rfmt.currency <- function(x, currency_symbol = "$", digits = 2, nsmall = digits,
     return(y)
 }
 
+
+## RFMT.ABBR() ABBREVIATES LARGE NUMBERS ##
+## BASED ON https://gist.github.com/RadGH/84edff0cc81e6326029c#gistcomment-2095834 ##
+Rfmt.abbr <- function(n) {
+    if (n > 0 & n < 1000) {
+        ## 1 - 999 ##
+        n_format = floor(n);
+        suffix = '';
+    } else if (n >= 1000 & n < 1000000) {
+        ## 1k-999k ##
+        n_format = floor(n / 1000);
+        suffix = 'K+';
+    } else if (n >= 1000000 & n < 1000000000) {
+        ## 1m-999m ##
+        n_format = floor(n / 1000000);
+        suffix = 'M+';
+    } else if (n >= 1000000000 & n < 1000000000000) {
+        ## 1b-999b ##
+        n_format = floor(n / 1000000000);
+        suffix = 'B+';
+    } else if (n >= 1000000000000) {
+        ## 1t+ ##
+        n_format = floor(n / 1000000000000);
+        suffix = 'T+';
+    }
+    
+    res <- paste0(n_format, suffix)
+    return(res)
+}
