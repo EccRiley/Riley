6e870ba3d810c1e8fe1155d9ae171beca3f768c6
Riley Smith-Hunter <rms7@pdx.edu>
2018-10-18 18:16:56 -0400

    Update and rename Rfmt_currency.R to Rfmt.R

---
+++ R/Rfmt.R
@@ -0,0 +1,19 @@
+Rfmt <- function(x, digits = 2, nsmall = digits, scientific = FALSE, big.mark = ",", ...) {
+    if (!is.numeric(x) | !is.integer(x) | !is.double(x)) warning("Coercing non-numeric input (x) to numeric")
+    res <- format(round(as.numeric(x), digits = digits), ...,
+           big.mark = big.mark,
+           scientific = scientific,
+           nsmall = digits)
+    res <- ifelse(is.na(x), NA, res)
+    return(res)
+}
+
+Rfmt.currency <- function(x, currency_symbol = "$", digits = 2, nsmall = digits, 
+                          scientific = FALSE, big.mark = ",", ...) {
+    y <- paste0(currency_symbol,
+                Rfmt(x, digits = digits, ...,
+                     big.mark = big.mark,
+                     scientific = scientific,
+                     nsmall = digits))
+    return(y)
+}
--- R/Rfmt_currency.R
+++
@@ -1,9 +0,0 @@
-
-Rfmt_currency <- function(x, currency_symbol = "$", digits = 2, ...) {
-    y <- paste0(currency_symbol,
-                format(round(x, digits = digits), ...,
-                       big.mark = ",",
-                       scientific = FALSE,
-                       nsmall = digits))
-    return(y)
-}
