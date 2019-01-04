63a1d994573000aec8b948eb1f5d9de64d960ae2
Riley Smith-Hunter <rileymsmith19@gmail.com>
2019-01-04 09:23:10 -0500

    Create Rgrep.R

---
+++ R/Rgrep.R
@@ -0,0 +1,3 @@
+Rgrep <- function(pattern, x, ignore.case = FALSE, perl = TRUE, value = TRUE, ...) {
+    grep(pattern = pattern, x = x, ignore.case = ignore.case, perl = perl, value = value, ...)
+}
