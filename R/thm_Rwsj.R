5777eee08750fce5f9ab304cd84913ca0dcc729d
Riley Smith-Hunter <rileymsmith19@gmail.com>
2019-01-06 13:03:04 -0500

    Add thm_Rwsj2()

--- R/thm_Rwsj.R
+++ R/thm_Rwsj.R
@@ -1,17 +1,17 @@
 
-thm_Rwsj <- function(base_size = 10, base_family = "serif", title_family = "serif", bgcolor = "white",
-                     title = element_text(size = rel(1.25), color = "#474747", face = "bold"),
-                     axis.title = element_text(size = rel(1), face = "bold", color = "#474747"),
+thm_Rwsj <- function(base_size = 10, base_family = "serif", title_family = "serif", bgcolor = "transparent",
+                     title = element_text(size = rel(1.25), color = "#252525", face = "bold"),
+                     axis.title = element_text(size = rel(1), face = "bold", color = "#252525"),
                      axis.text = element_text(size = rel(0.75), face = "plain"),
                      legend.position = "bottom", 
                      legend.direction = "horizontal", 
-                     legend.title = element_text(size = rel(1), face = "bold.italic", color = "#474747"),
+                     legend.title = element_text(size = rel(1), face = "bold.italic", color = "#252525"),
                      legend.text = element_text(size = rel(1)),
                      legend.key = element_rect(color = NA),
                      legend.key.size = unit(0.5, "cm"), 
                      strip.background = element_rect(fill = "#e3e3e6"),
                      strip.text = element_text(face = "bold"),
-                     rect = element_rect(fill = NA, size = 0.5, linetype = 1, colour = "#474747"),
+                     rect = element_rect(fill = "transparent", size = 0.5, linetype = 1, colour = "#252525"),
                      ...) {
     require(ggplot2); require(ggthemes);
     thm <- theme_wsj(base_size, base_family, title_family, color = bgcolor) %+replace% 
@@ -29,5 +29,34 @@ thm_Rwsj <- function(base_size = 10, base_family = "serif", title_family = "seri
               rect = rect,
               # panel.background = element_rect(color = "#474747", size = 1),
               ...)
+    thm
+}
+
+thm_Rwsj2 <- function() {
+    thm <- thm_Rwsj(base_family = "sans", title_family = "sans", 
+                    bgcolor = "transparent",
+                    plot.title = element_text(color = "#252525", size = rel(1.25),
+                                              face = "bold", hjust = 0.5, vjust = 0),
+                    legend.title = element_text(size = rel(1), color = "#252525"),
+                    legend.text = element_text(size = 12, color = "#252525"),
+                    legend.key.size = unit(0.5, "cm"),
+                    legend.position = "right", legend.direction = "vertical",
+                    axis.text.x = element_text(size = 12, hjust = 0.5, color = "#252525"),
+                    axis.text.y = element_text(size = 12, hjust = 1, color = "#252525"),
+                    axis.title.x = element_text(size = 12),
+                    axis.title.y = element_blank(),
+                    ## ... ##
+                    legend.box.margin = margin(0, 0, 0, 0, "pt"),
+                    legend.margin = margin(0, 0, 0, 0, "pt"),
+                    legend.background = element_blank(),
+                    legend.box.background = element_blank(),
+                    plot.background = element_blank(),
+                    panel.border = element_blank(),
+                    panel.grid.major = element_line(
+                        colour = adjustcolor("lightgray", alpha.f = 0.5),
+                        linetype =  "dotted", size = rel(1.5)),
+                    panel.grid.minor = element_line(
+                        colour = adjustcolor("lightgray", alpha.f = 0.5),
+                        linetype =  "dotted", size = rel(1))) 
     thm
 }
