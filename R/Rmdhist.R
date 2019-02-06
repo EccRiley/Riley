8de93c909b301f10dfd85ddf40970f9ce5ca292d
Riley Smith-Hunter <rileymsmith19@gmail.com>
2019-02-06 15:44:42 -0500

    Update Rmdhist.R

--- R/Rmdhist.R
+++ R/Rmdhist.R
@@ -1,9 +1,10 @@
-Rmdhist <- function(x, width = ceiling(sqrt(ncol(x))), length = ceiling(ncol(x)/width), pal = mpal(seq(1:ncol(x)), p = ppal), labs = colnames(x), xlab = labs, main = NULL, plotTitle = NULL, outerTitle = TRUE, ...) {
-    par(mfrow = c(length, width))
+Rmdhist <- function(x, width = ceiling(sqrt(ncol(x))), height = ceiling(ncol(x)/width), pal = mpal(seq(1:ncol(x)), p = ppal), border = "#3e3e3e", lwd = 1, lty = "dotted", col.lines = "#000000", labs = colnames(x), xlab = labs, main = NULL, plotTitle = NULL, outerTitle = TRUE, ...) {
+    opar <- par(no.readonly = TRUE) 
+    par(mfrow = c(height, width))
     for (i in 1:ncol(x)) {
-        hist(x[, i], col = pal[i], freq = FALSE, main = main[i], xlab = xlab[i], ...);
-        lines(density(x[, i], na.rm = TRUE), lwd = 1, lty = "dotted", ...)
+        hist(x[, i], col = pal[i], border = border, freq = FALSE, main = main[i], xlab = xlab[i], ...);
+        lines(density(x[, i], na.rm = TRUE), lwd = lwd, lty = lty, col = col.lines)
         if (!is.null(title)) title(plotTitle, outer = outerTitle)
     }
-    on.exit(par(mfrow = c(1, 1))) ## NEED TO FIX THIS ##
+    on.exit(par(opar))
 }
