aaa77158c6ccde04d4978b07e3e02d05310b07ec
Riley Smith-Hunter <rms7@pdx.edu>
2019-04-03 12:16:38 -0400

    Create Rkable_format.R

---
+++ R/Rkable_format.R
@@ -0,0 +1,24 @@
+
+Rkable_format <- function(interactive_format = "html") {
+    if (interactive()) {
+        res <- interactive_format
+    } else if (is_html_output()) {
+        res <- "html"
+    } else if (is_latex_output()) {
+        res <- "latex"
+    } else {
+        res <- getOption("knitr.table.format")
+    }
+    return(res)
+}
+
+Rkable <- function(..., format = Rkable_format(), kable_styling_args = NULL) {
+    require(knitr); require(kableExtra)
+    if (format == "html") {
+        kable(..., format = format) %>% kable_styling(unlist(kable_styling_args))
+    }
+}
+
+#' # Examples
+#' 
+# Rkable(mtcars, kable_styling_args = alist(bootstrap_options = "striped"))
