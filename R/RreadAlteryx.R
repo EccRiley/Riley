4ac41d10e16a292c3658473213f5ef52dbedba65
Riley Smith-Hunter <rileymsmith19@gmail.com>
2020-04-02 13:39:26 -0400

    Update RreadAlteryx.R

--- R/RreadAlteryx.R
+++ R/RreadAlteryx.R
@@ -11,14 +11,14 @@ RreadAlteryx <- function(input_id = "#1", inmode = "data.frame",
     str(dat0)
 
     vars_chr <- c(names(which(sapply(dat0, is.factor))))
-    dat01[, (vars_chr) := lapply(.SD, as.character), .SDcols = c(vars_chr)]
+    dat0[, (vars_chr) := lapply(.SD, as.character), .SDcols = c(vars_chr)]
 
-    str(dat01[, .SD, .SDcols = c(vars_chr)])
+    str(dat0[, .SD, .SDcols = c(vars_chr)])
     str(dat0[, .SD, .SDcols = c(vars_chr)])
 
     if (trimchars == TRUE) {
-        dat01[, (vars_chr) := lapply(.SD, stringr::str_trim), .SDcols = c(vars_chr)]
-        dat01[, (vars_chr) := lapply(.SD, function(x) ifelse(x == "", NA, as.character(x))),
+        dat0[, (vars_chr) := lapply(.SD, stringr::str_trim), .SDcols = c(vars_chr)]
+        dat0[, (vars_chr) := lapply(.SD, function(x) ifelse(x == "", NA, as.character(x))),
             .SDcols = c(vars_chr)]
     }
     return(dat0)
