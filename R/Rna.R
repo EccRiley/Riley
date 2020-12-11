f0888d576fdb42a3d3dd7e6806b838abb40ae6d0
Riley Smith-Hunter <16991907+EccRiley@users.noreply.github.com>
2020-12-11 14:48:00 -0500

    Add and implement include_inf argument to Rna.R

--- R/Rna.R
+++ R/Rna.R
@@ -1,7 +1,17 @@
 #' ## **`Rna()`**
 #'
 #' SURELY THIS ALREADY EXISTS SOMEWHERE, RIGHT???
-Rna <- function(x, v = 0) {
-    x <- ifelse(is.na(x), v, x)
-    x
+# Rna <- function(x, v = 0) {
+#     x <- ifelse(is.na(x), v, x)
+#     x
+# }
+
+Rna <- function (x, v = 0, include_inf = TRUE) {
+    if (include_inf == TRUE) {
+        x <- ifelse(is.na(x) | is.nan(x) | is.infinite(x), v, x)
+    } else {
+        x <- ifelse(is.na(x) | is.nan(x), v, x)
+    }
+    
+    return(x)
 }
