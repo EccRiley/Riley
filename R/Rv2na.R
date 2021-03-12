d5eed23063bdd129c2ad65ef2d618b2d89e106e3
Riley Smith-Hunter <16991907+EccRiley@users.noreply.github.com>
2021-03-12 10:59:45 -0500

    Update Rv2na.R

--- R/Rv2na.R
+++ R/Rv2na.R
@@ -1,4 +1,11 @@
-R.v2na <- function(x, v = 0) {
-	x <- ifelse(x == v, NA, x)
-	return(x)
+Rv2na <- function(x, v = 0, alt_replacement = NULL) {
+    y <- x
+    if (is.null(alt_replacement)) {
+        res <- ifelse(is.character(y) & y == v, NA_character_, 
+            ifelse(!is.character(y) & y == v, NA, y))
+    } else {
+        res <- ifelse(y == v, alt_replacement, y)
+    }
+    
+    return(res)
 }
