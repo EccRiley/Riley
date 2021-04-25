48cd25fe904ea267b0b3d54ae59651138fd07cbf
Riley Smith-Hunter <16991907+EccRiley@users.noreply.github.com>
2021-04-25 16:39:54 -0400

    Create Rpostread.R

---
+++ R/Rpostread.R
@@ -0,0 +1,21 @@
+Rpostread <- function(x) {
+    library(magrittr); library(purrr); library(data.table)
+    
+    y <- x %>%
+        purrr::map_if(is.character, function(x) {
+            ## REPLACE ANY NON UTF-8 CHRs W/ '' ##
+            Encoding(x) <- "UTF-8"
+            return(iconv(x, "UTF-8", "UTF-8", sub = ''))
+        }) %>%
+        purrr::map_if(is.character, stringr::str_trim) %>%
+        purrr::map_if(is.character,
+            function(x) ifelse(
+                x %in% c(getOption("datatable.na.strings"), ""), 
+                NA_character_,
+                toupper(gsub("[_]+", "_",
+                    gsub("([ ]+)|([\\-]+)", "_", x))))) %>%
+        as.data.table() %>%
+        unique() %>%
+        setnames(., names(.), tolower(names(.)))
+    return(y)
+}
