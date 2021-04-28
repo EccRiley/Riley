75a0af655d74d3d235509a16ceff230ce00efb5b
Riley Smith-Hunter <16991907+EccRiley@users.noreply.github.com>
2021-04-28 14:54:12 -0400

    Update Ryearmo.R

--- R/Ryearmo.R
+++ R/Ryearmo.R
@@ -1,6 +1,15 @@
 ## CONVENIENCE FUNCTION ##
 
-Ryearmo <- function(x) {
+Ryearmo <- function(x, return_integer = TRUE, sep = "", ...) {
     library(data.table)
-    return(as.integer(format(x, "%Y%m")))
+    y0 <- format(x, paste0("%Y", sep, "%m"), ...)
+    if (sep == "" & return_integer == TRUE) {
+        y <- as.integer(y0)
+    } else {
+        y <- y0
+    }
+    return(y)
 }
+
+# Ryearmo(today())
+# Ryearmo(today(), sep = "-")
