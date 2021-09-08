6bb08cfc9e54977d3f6eb92ab30d2cbf7d9a6199
Riley Smith-Hunter <16991907+EccRiley@users.noreply.github.com>
2021-09-08 16:19:50 -0400

    Create Rrescale_mm.R

---
+++ R/Rrescale_mm.R
@@ -0,0 +1,19 @@
+Rrescale_mm <- function(x, inverse = FALSE, na.rm = TRUE) {
+    minx <- min(x, na.rm = na.rm)
+    maxx <- max(x, na.rm = na.rm)
+    if (inverse == TRUE) {
+        y_top <- abs(minx - x)
+        y_botm <- abs(minx - maxx)
+    } else {
+        y_top <- x - minx
+        y_botm <- maxx - minx
+    }
+
+    y0 <- y_top/y_botm
+    if (inverse == TRUE) {
+        y <- 1 - y0
+    } else {
+        y <- y0
+    }
+    return(Y)
+}
