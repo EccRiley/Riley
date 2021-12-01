1c8795fc1e562b66b0df9fabb83067cd9a5b26a5
Riley Smith-Hunter <16991907+EccRiley@users.noreply.github.com>
2021-12-01 11:52:38 -0500

    Update Rkable.R

--- R/Rkable.R
+++ R/Rkable.R
@@ -5,7 +5,8 @@ Rkable <- function (x, ..., format = Rkable_format(),
     responsive = TRUE, 
     hover = TRUE,
     latex_options = NULL, 
-    protect_latex = TRUE) 
+    protect_latex = TRUE,
+    htmltable_class = "lightable-classic") 
 {
     library(knitr)
     library(kableExtra)
@@ -17,5 +18,6 @@ Rkable <- function (x, ..., format = Rkable_format(),
         bootstrap_options = bo, 
         latex_options = latex_options, 
         fixed_thead = list(enabled = fixed_thead_enabled, background = fixed_thead_bg), 
-        protect_latex = protect_latex)
+        protect_latex = protect_latex,
+        htmltable_class = htmltable_class)
 }
