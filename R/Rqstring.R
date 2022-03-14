e6145a3caf8427cb3baa4b64724010f2b586b9f4
Riley Smith-Hunter <16991907+EccRiley@users.noreply.github.com>
2022-03-14 11:42:14 -0400

    Update Rqstring.R

--- R/Rqstring.R
+++ R/Rqstring.R
@@ -1,7 +1,15 @@
 ## SIMPLE UTIL FOR QUERY BUILDING ##
-Rqstring <- function(x, field, ...) {
-    qstring <- paste0("AND LTRIM(RTRIM(", field, ")) IN ('",
-        paste0(unique(c(as.character(unique(x)))), collapse = "','"), "')",
-        ...)
+Rqstring <- function(x, field, substring_side = NULL, substring_n = NULL, ...) {
+    if (!is.null(substring_n) & substring_side %in% c("LEFT", "RIGHT")) {
+        qstring <- paste0("AND ", substring_side, "(LTRIM(RTRIM(", field, ")), ", 
+            substring_n, ") IN ('",
+            paste0(unique(c(as.character(unique(x)))), collapse = "','"), "')",
+            ...)
+    } else {
+        qstring <- paste0("AND LTRIM(RTRIM(", field, ")) IN ('",
+            paste0(unique(c(as.character(unique(x)))), collapse = "','"), "')",
+            ...)
+    }
     return(qstring)
 }
+
