7ec8e2220522770bf4518b25f35a85f4418fa9f7
Riley Smith-Hunter <16991907+EccRiley@users.noreply.github.com>
2022-07-17 20:17:52 -0400

    Delete Rnumwrd.R

--- R/Rnumwrd.R
+++
@@ -1,9 +0,0 @@
-Rnumwrd <- function(x, cap = FALSE) {
-    numWords <- c("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-    if (cap == TRUE) {
-        numWords <- sapply(numWords, RtCap, USE.NAMES = FALSE)
-    }
-    if (x > length(numWords) || (x %% 1) != 0) { stop("'x' is not a single-digit whole number") }
-    res <- numWords[x]
-    return(res)
-}
